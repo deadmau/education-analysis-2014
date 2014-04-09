acc1b865be260f5d999801b80bf3cf018a79f148
Ki Hyun Won <kithwon@naver.com>
2014-04-08 22:33:24 -0700

    twitter_result.py supports csv

--- twitter_result.py
+++ twitter_result.py
@@ -1,4 +1,6 @@
 from TwitterSearch import *
+import csv
+
 try:
     #create a TwitterSearch object
     tso = TwitterSearchOrder() 
@@ -17,18 +19,28 @@ try:
                        access_token = '2419638589-5THAmW79kgGzcF3USxmi4kK0KA4UiUBzCMFxI6x',
                        access_token_secret = 'Pkb4ZPHODZ4ucDoKMPtzjbehlffIX6dUj84Yt5UmJbOGU'
                        )
-
-    #create a file to save search results
-    result = open('twitter_sentiment.txt', 'w+')
+    #initialize field name for each column
+    field = ['year', 'text', 'city']
+    #where search results are initially stored
+    data = []
     #counter for how many tweets 
     counter = 0
     #write all the search results in result.txt
     for tweet in ts.searchTweetsIterable(tso):
-        result.write( '%s \t %s \t %s \n' % ( tweet['user']['created_at'].encode('ascii', 'ignore'), tweet['text'].encode('ascii', 'ignore'), tweet['user']['location'].encode('ascii', 'ignore')) )
+        value = [tweet['user']['created_at'][-4:], tweet['text'].encode('ascii', 'ignore'), tweet['user']['location'].encode('ascii', 'ignore')]
+        data.append(dict(zip(field, value)))
         counter += 1
-    result.write('Total tweets: '+str(counter))
-    #close file
-    result.close()
+    
+    #create a file to save search results
+    with open('twitter_sentiment.csv', 'w+') as result:
+            # create the csv writer object
+            tweetwriter = csv.DictWriter(result, delimiter=',', fieldnames=field)
+            tweetwriter.writeheader()
+            for row in data:
+                tweetwriter.writerow(row)
+
+    #print out the total tweets
+    print 'Total tweets: '+str(counter)
 
 #throw exception if anything goes wrong
 except TwitterSearchException as e:
